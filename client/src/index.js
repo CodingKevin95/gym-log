05bb35d6f2a482256ea928cd9975faf1a2f9ff2e
Kevin Son <kevinson95@gmail.com>
2022-08-29 10:20:25 -0400

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import CreatePost from "./pages/CreatePost"
+import Navbar from "./components/Navbar";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
+    </React.StrictMode>
+    </BrowserRouter>,
+  document.getElementById("root")
+);
