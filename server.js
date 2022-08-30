188b35e92689271b38481c646d1939b76bec2b1d
Kevin Son <kevinson95@gmail.com>
2022-08-29 20:22:16 -0400

    debug

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI).catch((err) => console.log(err));
+mongoose.connect('mongodb+srv://codingkevin95:kevin123@cluster0.eg9hmxt.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true
+})
+.catch((err) => console.log(err));
 
 //DB Schema and model
 const postSchema = mongoose.Schema({
